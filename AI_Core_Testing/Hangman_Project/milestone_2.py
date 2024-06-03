230efb44ec28dc5ece0bb8d49a47588d79f684ec
gordondms <gordon.murraysmith@gmail.com>
2024-06-03 15:13:29 +0100

    Code refactored

--- AI_Core_Testing/Hangman_Project/milestone_2.py
+++ AI_Core_Testing/Hangman_Project/milestone_2.py
@@ -5,12 +5,12 @@ word_list=["Apple", "Banana", "Strawberry", "Melon", "Kiwi"]
 print(word_list)
 
 # Select a random word
-word = random.choice(word_list)
-print(word)
+random_word = random.choice(word_list)
+print(random_word)
 
 # User input - guess a letter
-guess = input("Please enter a letter: ")
-if (len(guess) == 1) and (str.isalpha(guess) == True):
+user_guess = input("Please enter a letter: ")
+if (len(user_guess) == 1) and (str.isalpha(user_guess) == True):
     print("Good guess!")
 else:
     print("Oops! That is not a valid input")
