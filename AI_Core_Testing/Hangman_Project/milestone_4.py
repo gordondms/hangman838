2f107b734064617e5c6f8964733193fcf6b6c13d
gordondms <gordon.murraysmith@gmail.com>
2024-06-04 22:24:26 +0100

    Code refactored

--- AI_Core_Testing/Hangman_Project/milestone_4.py
+++ AI_Core_Testing/Hangman_Project/milestone_4.py
@@ -4,7 +4,7 @@ import random
 word_list = ["Apple", "Banana", "Strawberry", "Melon", "Kiwi"]
 
 # Select a random word
-word = random.choice(word_list)
+word = str.lower(random.choice(word_list))
 
 # Define variables
 
@@ -12,6 +12,7 @@ word_guessed = ["_"] * len(word)
 num_letters = len(set(word))
 list_of_guesses = []
 
+# Print variables for code testing only
 print(word)
 print(num_letters)
 
@@ -28,8 +29,12 @@ class Hangman:
 
     # Method1: Checks if the users guess is a letter within the random word
     def check_guess(self, guess):
+        """Converts guessed letter to lower case"""
         guess = str.lower(guess)
-        # If guess is correct user gets confimation and the blank spaces in word_guessed are populated with the correct letter
+        """
+        If guess is correct user gets confimation and the blank spaces in word_guessed are populated with the correct letter
+        If guess in not correct the num_lives variable is reduced by 1 and the user is informed the guess was not correct and how many lives they have left
+        """
         if guess in word:
             print("Good guess!", guess, "is in the word.")
             for index, letter in enumerate(word):
@@ -43,10 +48,18 @@ class Hangman:
     num_letters -= 1
     print(num_letters)    
     
-    # Method2: 
+    # Method2: Request user input, user guesses a letter
+    #          Checks validity of user input
+    #          Checks if user has already attempted that letter
     def ask_for_input(self):
         while True:
+            """User inputs a letter and this is stored in the guess variable"""
             guess = input("Please enter a letter: ")
+            """
+            Checks validity of letter inputted
+            Checks if letter has previously been inputted
+            Updates list_of_guesses variable to show all user guesses so far and references the check_guess method
+            """
             if (len(guess) != 1) or (str.isalpha(guess) == False):
                 print("Invalid letter. Please, enter a single alphabetical character.")
             elif guess in list_of_guesses:
